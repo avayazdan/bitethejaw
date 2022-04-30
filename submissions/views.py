2ee39292d24d3aaaf477154f9794060c3798a565
Ava Yazdany <avayaz1337@gmail.com>
2022-04-30 12:03:38 +0200

    added generics

--- submissions/views.py
+++ submissions/views.py
@@ -1,13 +1,12 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-
+from rest_framework import generics
 from .models import Submissions
 from .serializers import SubmissionSerializer
 
+class SubmissionsListView(generics.ListCreateAPIView):
+    queryset = Submissions.objects.all()
+    serializer_class = SubmissionSerializer
 
-class SubmissionsListView(APIView):
-    # `_request` is not used. The leading underscore expresses that it won't be used.
-    def get(self, _request):
-        submissions = Submissions.objects.all()
-        serialized_shows = SubmissionSerializer(submissions, many=True)
-        return Response(serialized_shows.data)
+class SubmissionsDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Submissions.objects.all()
+    serializer_class = SubmissionSerializer
+
