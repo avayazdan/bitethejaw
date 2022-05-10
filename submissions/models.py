2eb933e8280c141085892efb8c7fcbc72a217623
Ava Yazdany <avayaz1337@gmail.com>
2022-05-10 23:44:59 +0200

    added new changes to models

--- submissions/models.py
+++ submissions/models.py
@@ -5,12 +5,12 @@ from django.db import models
 from users.models import User
 
 CATEGORIES = [
-    ('at', "Art"),
-    ('hi', "History"),
-    ('ph', "Philosophy"),
-    ('po', "Politics"),
-    ('pm', "Poetry"),
-    ('ms', "Misc")
+    ('art', "Art"),
+    ('history', "History"),
+    ('philosophy', "Philosophy"),
+    ('politics', "Politics"),
+    ('poetry', "Poetry"),
+    ('film_studies', "Film studies")
 ]
 
 
@@ -22,13 +22,14 @@ class Submissions(models.Model):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     # submission_id = models.PositiveIntegerField(primary_key=True)
     # uuid = models.UUIDField(default=uuid.uuid4, unique=True)
+    title = models.CharField(max_length=200, default=None, null=True)
     image = models.CharField(max_length=200, default=None)
     submitted_by = models.ForeignKey(
         settings.AUTH_USER_MODEL, blank=True, on_delete=models.CASCADE)
     # submitted_by = models.ForeignKey(
     #     User, related_name="submitted_by", on_delete=models.PROTECT)
     date_submitted = models.DateField(auto_now_add=True)
-    text_field = models.CharField(max_length=2000, default=None)
+    text_field = models.CharField(max_length=6000, default=None)
     category = models.TextField(
         choices=CATEGORIES,
         default=None,
--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ class User(AbstractUser):
 
     date_joined = models.DateField(auto_now_add=True)
     email = models.CharField(max_length=50, default=None)
-    bio = models.CharField(max_length=300, default="Add your bio here")
+    bio = models.CharField(max_length=1000, default="Add your bio here")
     display_picture = models.CharField(
         max_length=200, blank=True, default="This user doesn't have a display picture")
 
